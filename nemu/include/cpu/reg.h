d41cf0e817431b1c19375598d73b1fcc76580d37
tracer-ics2016 <tracer@njuics.org>
2016-09-04 01:19:18 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:19:18 up  4:11,  1 user,  load average: 0.08, 0.02, 0.01 bbb83708a33cf148bfb652d2df416646f98801f9

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -16,7 +16,6 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
 typedef struct {
     union{
-    union{
 	union {
 		uint32_t _32;
 		uint16_t _16;
@@ -26,7 +25,6 @@ typedef struct {
 	/* Do NOT change the order of the GPRs' definitions. */
     union{
 	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};
-    };
     
 	swaddr_t eip;
 
