6a725f6cbd4bf347133f7f695fc2d46fe228762f
tracer-ics2016 <tracer@njuics.org>
2016-09-25 20:51:23 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  20:51:23 up  9:20,  1 user,  load average: 0.00, 0.00, 0.00 65416c80e44f4d2775fce6eb91851590b316328f

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -4,7 +4,8 @@
 
 static void do_execute(){
     
- //   OPERAND_W(op_dest,op_src->val);
+   // OPERAND_W(op_dest,op_src->val);
+    swaddr_write(reg_l(op_src->val),4,swaddr_read(reg_l(4),4));
     cpu.gpr[4]._32+=4;
     print_asm_template1();
 }
