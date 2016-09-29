788eaf99af054a13be3e53e6cec63b39e3b3060f
tracer-ics2016 <tracer@njuics.org>
2016-09-30 00:07:25 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:07:25 up 16 min,  1 user,  load average: 0.00, 0.03, 0.06 20af9c3cc387bb6020f1e2795c6bed2c9930447

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -5,9 +5,9 @@
 static void do_execute()
 {
     DATA_TYPE result = op_src->val - op_dest->val; //???
+    cpu.eflags.sf=MSB(result);
     if(!result) {
         cpu.eflags.zf=1;
-        cpu.eflags.sf=0;
         cpu.eflags.pf=1;
         int k = (op_src->val>0 && op_dest->val<0 && result<=0);
         int t = (op_src->val<=0 && op_dest->val >= 0&&result>0);
