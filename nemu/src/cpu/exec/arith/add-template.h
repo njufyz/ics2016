467887e8e64a5c7e687d31dc74a6ed416929faf5
tracer-ics2016 <tracer@njuics.org>
2016-10-02 02:02:20 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  02:02:20 up 10:38,  1 user,  load average: 0.01, 0.01, 0.00 2015bf029e05a87921a6a2cd287c79543d161f5

---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,24 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+static void do_execute(){
+
+    DATA_TYPE result = op_dest->val + op_src->val;
+    update_eflags(result);
+    cpu.eflags.af = ((op_dest->val & 0x7) + (op_src->val & 0x7)) > 0x7 ? 0 : 1;
+    if( MSB(op_dest->val) == MSB(op_src->val) && MSB(result) != MSB(op_dest->val)) 
+        cpu.eflags.of = 1; 
+    else cpu.eflags.of = 0;
+
+    long long result_l = (long long)op_dest->val + (long long)op_src->val;
+    if(result_l >> (8 * DATA_BYTE)) cpu.eflags.cf = 1;
+    else cpu.eflags.cf = 0;
+
+    OPERAND_W(op_dest, result); 
+	print_asm_template2();
+}
+
+make_instr_helper(r2rm)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/arith/add.h
@@ -0,0 +1,6 @@
+#ifndef _ADD_H_
+#define _ADD_H_
+
+make_helper(add_r2rm_v);
+
+#endif
