40539c53bcf2183889f40cc64a5f27c9df59db51
tracer-ics2016 <tracer@njuics.org>
2016-10-01 12:49:42 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:49:42 up 9 min,  1 user,  load average: 0.00, 0.09, 0.09 338c4b007bdc7d68c734e41fb6ae88ed3b7ff9e

---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,12 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+static void do_execute(){
+    OPERAND_W(op_dest,op_dest->val-op_src->val);
+    print_asm_template2();
+}
+
+make_instr_helper(rm_imm)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/arith/sub.h
@@ -0,0 +1,8 @@
+#ifndef _SUB_H_
+#define _SUB_H_
+
+#include "cpu/exec/helper.h"
+
+make_helper(rm_imm_b);
+
+#endif
