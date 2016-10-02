d963f7d175160b23075240ac81b7fbcef0d8ae59
tracer-ics2016 <tracer@njuics.org>
2016-10-03 01:58:04 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:58:04 up 23:09,  1 user,  load average: 0.00, 0.00, 0.00 4ad13ab2d1721650c107373c21f9dac8d61acccf

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -6,7 +6,7 @@ static void do_execute(){
     if(cpu.eflags.zf==0) 
         OPERAND_W(op_src,1);
     else OPERAND_W(op_src,0);
-    print_asm_template2();
+    print_asm_template1();
 }
 
 make_instr_helper(rm)
