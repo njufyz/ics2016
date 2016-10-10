b86ab5e3c2446fb5307404502489b187985f6f94
tracer-ics2016 <tracer@njuics.org>
2016-10-10 11:06:17 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:06:17 up  3:54,  1 user,  load average: 0.00, 0.00, 0.00 60472dc5a9bf16e89e913a52bcfa7028a8c13950

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -16,7 +16,7 @@
 
 make_helper_v(jle_i)
 make_helper_v(jge_i)
-
+make_helper_v(jl_i)
 
 
 
--- nemu/src/cpu/exec/control/jcc.h
+++ nemu/src/cpu/exec/control/jcc.h
@@ -5,10 +5,13 @@ make_helper(je_i_b);
 make_helper(jbe_i_b);
 make_helper(jne_i_b);
 make_helper(jle_i_b);
-make_helper(jle_i_v);
 make_helper(jg_i_b);
 make_helper(jl_i_b);
-
 make_helper(jge_i_b);
+
+make_helper(jle_i_v);
 make_helper(jge_i_v);
+make_helper(jl_i_v);
+
+
 #endif
