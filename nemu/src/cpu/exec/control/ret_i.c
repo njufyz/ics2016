470bb21498ddd8f1a9daf57bbc35b368063519c9
tracer-ics2016 <tracer@njuics.org>
2016-10-12 00:15:27 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:15:27 up  8:08,  1 user,  load average: 0.01, 0.08, 0.03 5bb7ad17f185c72919639ab81457f1bf10fe8e

--- nemu/src/cpu/exec/control/ret_i.c
+++ nemu/src/cpu/exec/control/ret_i.c
@@ -5,7 +5,7 @@ make_helper(ret_i){
     cpu.eip = swaddr_read(cpu.gpr[4]._32,4);
     cpu.gpr[4]._32 += 4;
     uint32_t offset = instr_fetch(cpu.eip+1,2);
-    Log("%x",offset);
+    Log("%x",cpu.eip);
     cpu.eip = cpu.eip + offset -1;
     print_asm_template1();
     return 3;
