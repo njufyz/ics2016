2a20a362218b4da0ea955047efea0a5194e4ad83
tracer-ics2016 <tracer@njuics.org>
2016-10-13 00:29:38 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:29:38 up 15:47,  1 user,  load average: 0.01, 0.04, 0.01 102d19d794759afe61b59d581914a2b79ed6f56a

---
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -0,0 +1,19 @@
+#include "cpu/exec/template-start.h"
+
+#define instr scas
+
+make_helper(concat(scas_,SUFFIX)){
+    DATA_TYPE result = (DATA_TYPE)cpu.eax - MEM_R(cpu.edi);
+    long long result_l = (long long )cpu.eax - (long long)MEM_R(cpu.edi);
+    if(MSB((DATA_TYPE)cpu.eax) != MEM_R(cpu.edi) && MSB(result) != MSB((DATA_TYPE)cpu.eax))  cpu.eflags.of = 1;
+    else cpu.eflags.of = 0;
+    cpu.eflags.cf = (result_l << (8* DATA_BYTE)) & 1;
+    update_eflags(result);
+    if(cpu.eflags.df == 0)
+        cpu.edi +=DATA_BYTE;
+    else cpu.edi -=DATA_BYTE;
+    print_asm("scas%s",str(SUFFIX));
+    return 1;
+}
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/string/scas.h
@@ -0,0 +1,7 @@
+#ifndef _SCAS_H_
+#define _SCAS_H_
+
+make_helper(scas_b);
+make_helper(scas_v);
+
+#endif
