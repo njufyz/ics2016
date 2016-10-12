4a386fb07fc59fbb41c188022b2896832c11f080
tracer-ics2016 <tracer@njuics.org>
2016-10-13 01:08:29 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:08:29 up 16:26,  1 user,  load average: 0.23, 0.08, 0.02 68330382499cb79ef534b18f776f73691b34934

---
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+#define instr stos
+
+make_helper(concat(stos_,SUFFIX)){
+    swaddr_write(cpu.edi,DATA_BYTE,cpu.eax);
+    if(cpu.eflags.df == 0)
+        cpu.edi += DATA_BYTE;
+    else cpu.edi -= DATA_BYTE;
+    print_asm("stos%s",str(SUFFIX));
+    return 1;
+}
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/string/stos.h
@@ -0,0 +1,7 @@
+#ifndef _STOS_H_
+#define _STOS_H_
+
+make_helper(stos_b);
+make_helper(stos_v);
+
+#endif
