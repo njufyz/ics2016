e54c48571389c243dd891505f96a5986ccc397b1
tracer-ics2016 <tracer@njuics.org>
2016-10-14 01:57:01 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:57:01 up 1 day, 29 min,  1 user,  load average: 0.02, 0.08, 0.08 7afc48fd156ed216b185d4bb605823a09b556dfc

--- nemu/src/cpu/exec/control/leave.c
+++ nemu/src/cpu/exec/control/leave.c
@@ -2,9 +2,9 @@
 
 make_helper(leave)
 {
-    cpu.gpr[4]._32 = cpu.gpr[5]._32;
-    cpu.gpr[5]._32 = swaddr_read(cpu.gpr[4]._32,4);
-    cpu.gpr[4]._32+=4;
+    cpu.esp = cpu.ebp;
+    cpu.ebp = swaddr_read(cpu.esp,4);
+    cpu.esp +=4;
     print_asm("leave");
     return 1;
 }
