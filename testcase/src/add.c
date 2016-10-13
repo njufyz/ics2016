1cbded67255a7e6c68e86cc6ee62cd4d3bf57ebc
tracer-ics2016 <tracer@njuics.org>
2016-10-13 20:16:39 +0800

    >  run 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  20:16:39 up 20:25,  1 user,  load average: 0.14, 0.05, 0.01 bbbab18bd17c04d4de0b2ea0f6e81881a9ed60d6

--- testcase/src/add.c
+++ testcase/src/add.c
@@ -14,8 +14,6 @@ int b[8] = {0,1,2,0x7fffffff,0x80000000,0x80000001,0xfffffffe,0xffffffff};
 int main() {
 	int i, j, ans_idx = 0;
 	int loop = 0;
-    for(i=0;i<NR_DATA;i++)
-        nemu_assert(test_data[i]==b[i]);
 
 	for(i = 0; i < NR_DATA; i ++) {
 		for(j = 0; j < NR_DATA; j ++) {
