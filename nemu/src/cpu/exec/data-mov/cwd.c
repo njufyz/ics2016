bcdb77ac2b8ea6ff0b9e4265dfcf97d6b0b0d69e
tracer-ics2016 <tracer@njuics.org>
2016-10-14 14:02:45 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  14:02:45 up 1 day,  1:29,  1 user,  load average: 0.07, 0.05, 0.01 b0982d519640dc077a708132ec10b755826f555

--- nemu/src/cpu/exec/data-mov/cwd.c
+++ nemu/src/cpu/exec/data-mov/cwd.c
@@ -1,9 +1,9 @@
 #include "cpu/exec/helper.h"
 
 make_helper(cwd){
-    if(cpu.gpr[0]._32 <0) 
-        cpu.gpr[2]._32 = 0xffffffff;
-    else cpu.gpr[2]._32 = 0;
+    if(cpu.eax >> 31 == 1) 
+        cpu.edx = 0xffffffff;
+    else cpu.edx = 0;
     print_asm("cwd/cdq");
     return 1;
 }
