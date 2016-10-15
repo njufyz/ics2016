09f986e3d208caf184a083eac8d46e64c4475aa2
tracer-ics2016 <tracer@njuics.org>
2016-10-15 17:40:17 +0800

    >  compile NEMU 151220026 njufyz Linux 8ec1df0c9fcb 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  17:40:17 up 1 day,  8:31,  2 users,  load average: 0.00, 0.02, 0.00 83bb2eea8b845cfca1b74be5cb0a29fbff5e5604

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -3,9 +3,9 @@
 #define instr movzx
 
 static void do_execute(){
-    uint32_t result = op_src->val;
-    long long i =1;
-    result&=((i<<op_src->size * 8)-1);
+    uint32_t result =(DATA_TYPE) op_src->val;
+//    long long i =1;
+ //   result&=((i<<op_src->size * 8)-1);
    /* uint32_t result = temp; */ 
     write_operand_l(op_dest,result);
     print_asm_template2();
