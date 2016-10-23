bb8955f32360d4fed1df003a94dbe5d7dfc21589
tracer-ics2016 <tracer@njuics.org>
2016-10-24 00:31:44 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:31:44 up  2:21,  4 users,  load average: 0.01, 0.06, 0.08 aec37b2b62be6424194b9fb953e41c968ad38d3f

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -14,7 +14,7 @@ make_helper(rep) {
 			exec(eip + 1);
 			count ++;
 			cpu.ecx --;
-			assert(ops_decoded.opcode == 0xa4	// movsb
+		/*	assert(ops_decoded.opcode == 0xa4	// movsb
 				|| ops_decoded.opcode == 0xa5	// movsw
 				|| ops_decoded.opcode == 0xaa	// stosb
 				|| ops_decoded.opcode == 0xab	// stosw
@@ -23,7 +23,7 @@ make_helper(rep) {
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
 				);
-
+*/
 			/* TODO: Jump out of the while loop if necessary. */
             if(ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode ==0xae||ops_decoded.opcode ==0xaf)
             {
