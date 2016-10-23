34d115a1b5c9ffc7479542c65a0acc9152f2062e
tracer-ics2016 <tracer@njuics.org>
2016-10-23 23:42:38 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:42:38 up  1:32,  4 users,  load average: 0.00, 0.00, 0.00 8c21bdcc8f482f1ac3f9328891bdb45432592203

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,6 +3,8 @@
 #define instr movsx
 
 static void do_execute(){
+    if(!ops_decoded.is_operand_size_16) op_src->val &= 0xff;
+    else op_src->val &= 0xffff;
     DATA_TYPE_S temp = op_src->val;
     uint32_t result = temp;
    // uint32_t result = op_src->val;
