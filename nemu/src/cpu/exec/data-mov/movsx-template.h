ecfab7c77f82422f8d296c754e80bdb6c33bd885
tracer-ics2016 <tracer@njuics.org>
2016-10-23 23:45:52 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:45:52 up  1:35,  4 users,  load average: 0.00, 0.00, 0.00 e3c2e73d66a4e2a92951e639e6b39df6ef0fa489

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -7,11 +7,6 @@ static void do_execute(){
     else op_src->val &= 0xffff;
     DATA_TYPE_S temp = op_src->val;
     uint32_t result = temp;
-   // uint32_t result = op_src->val;
-   // if(MSB(result))
-  //    result = (0xffffffff<<(8*DATA_BYTE)) | op_src->val;
-   // uint32_t result = (DATA_TYPE_S) op_src->val;
-    
     OPERAND_W(op_dest,result);
     print_asm_template2();
 }
--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -4,15 +4,12 @@
 
 static void do_execute(){
     uint32_t result;
-    if(instr_fetch(cpu.eip+1,1)==0xb6)
+    if(ops_decoded.is_operand_size_16)
     {
          result =op_src->val & 0xff;   //bw
     }
     else
         result = op_src->val & 0xffff;
-//    long long i =1;
- //   result&=((i<<op_src->size * 8)-1);
-   /* uint32_t result = temp; */ 
    OPERAND_W(op_dest,result);
     print_asm_template2();
 }
