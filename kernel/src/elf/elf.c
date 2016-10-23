2a40ac54128d701594ca0fd632b508c9944f5d47
tracer-ics2016 <tracer@njuics.org>
2016-10-23 19:51:37 +0800

    >  compile kernel 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  19:51:37 up 3 days, 13:05,  4 users,  load average: 0.02, 0.05, 0.01 950238af32403b547406073a1c70b2ca4fb24f32

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,7 +31,7 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x7f454c46;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
     ph =(void *)(buf+ elf->e_phoff);
@@ -45,7 +45,7 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 	ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz); 
+			 	ramdisk_read((void*)(ph->p_vaddr), ph->p_offset, ph->p_filesz); 
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
