ac9c434d042bcc54ada18c0a028796a9f0c924e3
tracer-ics2016 <tracer@njuics.org>
2016-10-23 19:46:58 +0800

    >  compile kernel 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  19:46:58 up 3 days, 13:01,  4 users,  load average: 0.00, 0.02, 0.00 ea78c6fe1361655e61c5577a2536396beed5bcb

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,24 +31,26 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x7f454c46;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-
+    ph =(void *)(buf+ elf->e_phoff);
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	//panic("please implement me");
+    int i =0;
+	for(;i<elf->e_phnum;i++ ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+			 	ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz); 
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
+                memset((void*)(ph->p_vaddr + ph->p_filesz) , 0 , ph->p_memsz - ph->p_filesz);  
 
 
 #ifdef IA32_PAGE
