71b67fdb5ee1c1f8f56cb1e3c3966dbbd8ca98c9
tracer-ics2016 <tracer@njuics.org>
2016-10-27 17:42:54 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  17:42:54 up 21:30,  4 users,  load average: 0.02, 0.14, 0.08 b9dad5c0252f45149f68b86bc7d30560ca06ec32

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -17,7 +17,6 @@ FLOAT f2F(float a) {
     int v = *(int *)&a;
     int s = v >>31;
     int e = (v >> 23) & 0xff - 127;
-    int m = v & 0x7fffff;
     int r = (v&0x7fffff) + 0x800000;
     FLOAT result;
     result = (16+e-23)>0 ? (r<<(16+r-23)) : (r>>23-e-16);
