b5b8f567be9dc6822b391628455df1f9cacf3d5d
tracer-ics2016 <tracer@njuics.org>
2016-10-27 17:33:12 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  17:33:12 up 21:20,  4 users,  load average: 0.00, 0.00, 0.00 192446a664464ef7ae1ae28197cb6244ea7e63b

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -6,23 +6,7 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	/* Dividing two 64-bit integers needs the support of another library
-	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-	 * here. A "64/32" division is enough.
-	 *
-	 * To perform a "64/32" division, you can use the x86 instruction
-	 * `div' or `idiv' by inline assembly. We provide a template for you
-	 * to prevent you from uncessary details.
-	 *
-	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-	 *
-	 * If you want to use the template above, you should fill the "???"
-	 * correctly. For more information, please read the i386 manual for
-	 * division instructions, and search the Internet about "inline assembly".
-	 * It is OK not to use the template above, but you should figure
-	 * out another way to perform the division.
-	 */
+	 
   /*  FLOAT result;
     FLOAT remain;
     int t = ((a>>31)&1)?0xffffffff:0;
@@ -30,13 +14,13 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 
     return (result<<16)+remain;
     */
-    return 0;
-     int remainder[2];
+    // int remainder[2];
+     int r0,r1;
      int integer;
      int fraction;
      FLOAT r = 0;
-     asm volatile ("idivl %2" : "=a"(integer), "=d"(remainder[0]) : "r"((b)), "a"((a)), "d"(a>>31));
-     asm volatile ("idivl %2" : "=a"(fraction), "=d"(remainder[1]) : "r"((b)), "a"(remainder[0]<<16), "d"(remainder[0]>>31));
+     asm volatile ("idivl %2" : "=a"(integer), "=d"(r0) : "r"((b)), "a"((a)), "d"(a>>31));
+     asm volatile ("idivl %2" : "=a"(fraction), "=d"(r1) : "r"((b)), "a"(r0<<16), "d"(r0>>31));
      r += integer << 16;
      r += fraction;
      return r;
@@ -45,28 +29,10 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 
 
 FLOAT f2F(float a) {
-	/* You should figure out how to convert `a' into FLOAT without
-	 * introducing x87 floating point instructions. Else you can
-	 * not run this code in NEMU before implementing x87 floating
-	 * point instructions, which is contrary to our expectation.
-	 *
-	 * Hint: The bit representation of `a' is already on the
-	 * stack. How do you retrieve it to another variable without
-	 * performing arithmetic operations on it directly?
-	 */
     int v = *(int *)&a;
     int s = v >>31;
     int e = (v >> 23) & 0xff - 127;
     int m = v & 0x7fffff;
-   /* int result = 1;
-    int i=1;
-    for(;i<e+16+1;i++)
-    {
-        result = (result << 1) + ((m & (1<<22)) >> 22);
-        m <<= 1;
-    }
-     if(s!=0) result = -result;
-	return result;*/
     int r = (v&0x7fffff) + 0x800000;
     FLOAT result;
     result = (16+e-23)>0 ? (r<<(16+r-23)) : (r>>23-e-16);
