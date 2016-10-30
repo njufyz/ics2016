553659311c04ac31eb9be450a7739f477e08af95
tracer-ics2016 <tracer@njuics.org>
2016-10-31 00:47:43 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:47:43 up 1 day, 20:38,  4 users,  load average: 0.00, 0.01, 0.00 698737f3cd81b8c6bcab347a8afe27c647229be

--- testcase/src/print-FLOAT.c
+++ testcase/src/print-FLOAT.c
@@ -14,7 +14,7 @@ int main() {
 	printf("%f\n", FLOAT_ARG(0x00013333));
 	printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);
 #else
-	sprintf(buf, "%lld", FLOAT_ARG(0x00010000));
+	sprintf(buf, "%llu", FLOAT_ARG(0x00010000));
 	nemu_assert(strcmp(buf, "1.000000") == 0);
 	
 	sprintf(buf, "%f", FLOAT_ARG(0x00013333));
