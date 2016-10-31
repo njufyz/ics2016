b506a55aeb8dafc3dd205f1709cd550ff315bb23
tracer-ics2016 <tracer@njuics.org>
2016-11-01 00:53:09 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:53:09 up 2 days,  2:47,  3 users,  load average: 0.00, 0.00, 0.00 f5a11d9a7e45e52226256ecfcd5ba6c69ae6d570

--- testcase/src/print-FLOAT.c
+++ testcase/src/print-FLOAT.c
@@ -14,17 +14,17 @@ int main() {
 	printf("%f\n", FLOAT_ARG(0x00013333));
     printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);
 #else
-/*	sprintf(buf, "%f", FLOAT_ARG(0x00010000));
-	nemu_assert(strcmp(buf, "1.000000") == 0);
+	sprintf(buf, "%f %f", FLOAT_ARG(0x00010000),FLOAT_ARG(0x00013333));
+	nemu_assert(strcmp(buf, "1.000000 1.199996") == 0);
 	
 	sprintf(buf, "%f", FLOAT_ARG(0x00013333));
 	nemu_assert(strcmp(buf, "1.199996") == 0);
-*/
-/*	sprintf(buf, "%f %d", FLOAT_ARG(0xfffecccd), 123456);
-    nemu_assert(strcmp(buf, "-1.199996 123456") == 0);
-*/
-     sprintf(buf,"%d",123456);
-     nemu_assert(strcmp(buf,"123456")==0);
+
+//	sprintf(buf, "%f %d", FLOAT_ARG(0xfffecccd), 123456);
+//    nemu_assert(strcmp(buf, "-1.199996 123456") == 0);
+
+//     sprintf(buf,"%d",123456);
+//     nemu_assert(strcmp(buf,"123456")==0);
     
 //	sprintf(buf, "%d %f", 123456,FLOAT_ARG(0xfffecccd));
 //    nemu_assert(strcmp(buf, "123456 -1.199996") == 0);
