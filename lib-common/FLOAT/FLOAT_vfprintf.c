4e8d149063a82ff095c6613d7ffd134a4cfd9ab7
tracer-ics2016 <tracer@njuics.org>
2016-11-02 23:26:25 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:26:25 up  6:53,  4 users,  load average: 0.00, 0.01, 0.00 833696db63847e3ad78b511d9e6dcb5d8020890

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -214,8 +214,8 @@ static void modify_ppfs_setargs() {
 		++p;
 	}
 #endif
-//  uint32_t  p = (int)&_ppfs_setargs + 0x801148 - 0x8010d7;
-//  *(uint32_t*)p = 0x000042eb;
+  uint32_t  p = (int)&_ppfs_setargs + 0x801200 - 0x801194;
+  *(uint32_t*)p = 0x000047eb;
   
 }
 void init_FLOAT_vfprintf() {
