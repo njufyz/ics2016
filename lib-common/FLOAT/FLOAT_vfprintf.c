8ed284ec4d07e48a2ea4c137760391c4c59d4189
tracer-ics2016 <tracer@njuics.org>
2016-11-03 00:08:32 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:08:32 up  7:35,  3 users,  load average: 0.00, 0.00, 0.00 84516af7d89cce73238af699780ccc388eb5eec6

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -101,7 +101,7 @@ static void modify_vfprintf() {
        int o =(int) &_fpmaxtostr - (int)&format_FLOAT ;
        p = p + 1;
        int i = *(int *)p;
-//     mprotect((void *)((p-0x100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
+        mprotect((void *)((p-0x100) & 0xfffff000), 4096*2, PROT_READ | PROT_WRITE | PROT_EXEC);
       *(int*)p = *(int*)p - o;        //change call
       uint32_t sub = (uint32_t) &_vfprintf_internal + 0x80488df - 0x80485e6;
       *(uint32_t*)(sub) = 0xdb08ec83; //correct esp
