6ef258b1143dbb08af30a05286eff06fe93a01f5
tracer-ics2016 <tracer@njuics.org>
2016-11-03 01:33:48 +0800

    >  compile kernel 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:33:48 up  9:00,  3 users,  load average: 0.04, 0.17, 0.09 a64d437d9df2d52eb9726e38f97956e0203784

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -34,8 +34,9 @@ uint32_t loader() {
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-    ph =(Elf32_Phdr *)( elf->e_phoff);
-	/* Load each program segment */
+   // ph =(Elf32_Phdr *)( elf->e_phoff);
+     ph = (void *) elf->e_phoff;
+    /* Load each program segment */
 	//panic("please implement me");
     int i;
 	for(i=0;i<elf->e_phnum;i++,ph++ ) {
