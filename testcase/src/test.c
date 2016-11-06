0c17fbfbbed803c1d04e58280410fa91ee7e3619
tracer-ics2016 <tracer@njuics.org>
2016-11-06 23:22:24 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:22:24 up 16:06,  3 users,  load average: 0.00, 0.02, 0.03 5eec1ec37f0af40392306ea2a5c902766fb383ab

---
+++ testcase/src/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "FLOAT.h"
+
+#include "trap.h"
+
+char buf[128];
+
+int main()
+{
+        sprintf(buf,"%d",-1);
+            nemu_assert(strcmp(buf,"-1")==0);
+                return 0;
+}
+
