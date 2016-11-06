e42efd36a173ffa1cfe9e04787fc5f5ecced2fcd
tracer-ics2016 <tracer@njuics.org>
2016-11-06 19:03:51 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  19:03:51 up 15:46,  2 users,  load average: 0.00, 0.00, 0.00 52e49f434277cfdd4569758fab16cd5439ba5b9b

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -1,26 +1,26 @@
 #include "cpu/exec/template-start.h"
 
 #define instr push
-uint32_t eip;
 static void do_execute()
 {
-    if(DATA_BYTE==2)
-    {   
-        if(instr_fetch(eip,1)==0x6a)
+    if(ops_decoded.is_operand_size_16 && instr_fetch(cpu.eip+1,1)==0x6a)
         {
             int8_t t = op_src->val;
             op_src->val = t;
         }
-        cpu.esp-=DATA_BYTE;
+    else if(ops_decoded.is_operand_size_16 == 0 && instr_fetch(cpu.eip,1)==0x6a)
+        {
+            int8_t t = op_src->val;
+            op_src->val = t;
+        }
+
+    if(DATA_BYTE==2)
+    {   
+        cpu.esp-=2;
         swaddr_write(cpu.esp,2,op_src->val);
     }
     else
     {
-        if(instr_fetch(eip,1)==0x6a)
-        {
-            int8_t t = op_src->val;
-            op_src->val = t;
-        }
         cpu.esp-=4;
         swaddr_write(cpu.esp,4,op_src->val);
     }
