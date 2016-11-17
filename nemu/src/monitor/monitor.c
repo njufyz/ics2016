5b4910024742cca7bd7e70072a02bf28c4491228
tracer-ics2016 <tracer@njuics.org>
2016-11-17 23:37:30 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:37:30 up 1 day, 58 min,  3 users,  load average: 0.00, 0.01, 0.00 c459deee3d29698bd4b188a0e5a55effc98d877a

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -10,6 +10,7 @@ void load_elf_tables(int, char *[]);
 void init_regex();
 void init_wp_pool();
 void init_ddr3();
+void init_cache();
 
 FILE *log_fp = NULL;
 
@@ -103,4 +104,5 @@ void restart() {
     cpu.eflags.cf=0;
 	/* Initialize DRAM. */
 	init_ddr3();
+    init_cache();
 }
