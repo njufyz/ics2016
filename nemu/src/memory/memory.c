9cb28f0816f437225b0b16ae6701420dac8c1e14
tracer-ics2016 <tracer@njuics.org>
2016-11-19 20:22:33 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  20:22:33 up 1 day,  5:39,  3 users,  load average: 0.03, 0.03, 0.00 308f7be46d8ad45a10a8c27235a379b3874bf38f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -8,13 +8,13 @@ void cache_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	//return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-//	dram_write(addr, len, data);
-	cache_write(addr, len, data);
+	dram_write(addr, len, data);
+//	cache_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
