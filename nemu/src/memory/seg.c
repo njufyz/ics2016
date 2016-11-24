ec11e0b38bbe44dd0f44965ee65062d50a176cad
tracer-ics2016 <tracer@njuics.org>
2016-11-24 23:33:08 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:33:08 up 2 days, 12:03,  5 users,  load average: 0.00, 0.00, 0.00 6af21aef908a64d02ec7d3cc598af93349e010a9

--- nemu/src/memory/seg.c
+++ nemu/src/memory/seg.c
@@ -16,8 +16,8 @@ void load_segcache(uint8_t sreg){
             
         SegDesc *temp2 = (SegDesc *)m;
         /* assert check */
-        assert(temp2->present == 1);
-        assert(temp2->limit_15_0 + (temp2->limit_19_16 <<16) >= cpu.segreg[sreg].index * 8) ;
+       // assert(temp2->present == 1);
+      //  assert(temp2->limit_15_0 + (temp2->limit_19_16 <<16) >= cpu.segreg[sreg].index * 8) ;
         /* load seg */
         cpu.segcache[sreg].limit = temp2->limit_15_0 + (temp2->limit_19_16 <<16);
         cpu.segcache[sreg].base = temp2->base_15_0 + (temp2->base_23_16 << 16) +( temp2->base_31_24 <<24);
