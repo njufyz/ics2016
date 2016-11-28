e4b923c1e25e974d99fd0a9eaa86c8b6634c32e9
tracer-ics2016 <tracer@njuics.org>
2016-11-28 18:47:05 +0800

    >  compile kernel 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  18:47:05 up 3 days, 14:32,  2 users,  load average: 0.00, 0.00, 0.00 d8f70013add1dda0b6f7063f8e36c16267c84398

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -49,7 +49,7 @@ uint32_t loader() {
 			/*  zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-                memset((void*)(ph->p_vaddr + ph->p_filesz) , 0 , ph->p_memsz - ph->p_filesz);  
+                memset((void*)(hwaddr + ph->p_filesz) , 0 , ph->p_memsz - ph->p_filesz);  
 
 
 #ifdef IA32_PAGE
