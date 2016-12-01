b0a568dc42e4e7095753cd01514de308865ef3af
tracer-ics2016 <tracer@njuics.org>
2016-12-01 23:29:15 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:29:15 up  4:58,  2 users,  load average: 0.00, 0.00, 0.00 92dd7038165695599fd5dc58d45b496b60175bc2

--- nemu/src/cpu/exec/system/lgdt-template.h
+++ nemu/src/cpu/exec/system/lgdt-template.h
@@ -5,7 +5,7 @@
 static void do_execute(){
     Log("%x",op_src->addr);
     cpu.gdtr.limit = hwaddr_read(op_src->addr, 2);
-    cpu.gdtr.base = hwaddr_read(op_src->addr + 2, 4);
+    cpu.gdtr.base = hwaddr_read(op_src->addr , 4);
     print_asm("base: 0x%x limit: %x",cpu.gdtr.base,cpu.gdtr.limit);
 }
 
