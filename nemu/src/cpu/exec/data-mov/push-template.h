7f0b48953059f7b4b69379dcd13d56e81056cf31
tracer-ics2016 <tracer@njuics.org>
2016-12-03 01:52:29 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:52:29 up  2:40,  4 users,  load average: 0.06, 0.01, 0.00 7879a2de29270e35dab14e2c50a1a8ed45edda83

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -31,4 +31,37 @@ static void do_execute()
 make_instr_helper(i)
 make_instr_helper(r)
 make_instr_helper(rm)
+
+#if DATA_BYTE == 4
+make_helper(pushad){
+    uint32_t temp = cpu.esp;
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.eax, R_SS);
+    
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.ecx, R_SS);
+    
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.edx, R_SS);
+    
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.ebx, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, temp, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.ebp, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.esi, R_SS);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.edi, R_SS);
+
+    print_asm("pushad");
+    return 1;
+}
+#endif
 #include "cpu/exec/template-end.h"
