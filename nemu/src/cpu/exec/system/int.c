f93d0cca4bccb87536764fb81f7717b48c3250c4
tracer-ics2016 <tracer@njuics.org>
2016-12-03 23:11:32 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:11:32 up  9:22,  3 users,  load average: 0.08, 0.02, 0.01 cc712f7443ef6a1edd6e7bcda45b53c0d82188e4

--- nemu/src/cpu/exec/system/int.c
+++ nemu/src/cpu/exec/system/int.c
@@ -3,7 +3,7 @@
 void raise_intr(uint8_t);
 make_helper(int_imm8){
     uint8_t no = instr_fetch(cpu.eip + 1, 1);
-
+    cpu.eip += 2;
     raise_intr(no);
     print_asm("int %x", no);
     return 2;
