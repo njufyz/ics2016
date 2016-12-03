9f41300b3c9df150bc2faaaa295de3f3ef7baca7
tracer-ics2016 <tracer@njuics.org>
2016-12-03 16:27:57 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  16:27:57 up  8:21,  3 users,  load average: 0.07, 0.03, 0.01 31db555b2dca30ba8c377494b0fbac7f61609a85

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -1,4 +1,6 @@
 #include "cpu/exec/helper.h"
+void load_segcache(uint8_t);
+
 make_helper(ret)
 {
    cpu.eip = swaddr_read(cpu.esp, 4, R_SS);
@@ -14,6 +16,7 @@ make_helper(iret){
     cpu.eip -= 1;
 
     cpu.segreg[R_CS].val = swaddr_read(cpu.esp, 4, R_SS);
+    load_segcache(R_CS);
     cpu.esp += 4;
 
     cpu.eflags.val = swaddr_read(cpu.esp, 4, R_SS);
