82fbd2364f934e6d26ad5fe918aef2751ce5d5b5
tracer-ics2016 <tracer@njuics.org>
2016-12-03 16:47:50 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  16:47:50 up  8:41,  3 users,  load average: 0.00, 0.00, 0.00 f9bacd2ce7585fc02a591b1c38b04b8336937667

--- nemu/src/cpu/exec/system/int.c
+++ nemu/src/cpu/exec/system/int.c
@@ -3,15 +3,6 @@
 void raise_intr(uint8_t);
 make_helper(int_imm8){
     uint8_t no = instr_fetch(cpu.eip + 1, 1);
-   
-    cpu.esp -= 4;
-    swaddr_write(cpu.esp, 4, cpu.eflags.val, R_SS);  //push eflags
-    
-    cpu.esp -= 4;
-    swaddr_write(cpu.esp, 4, cpu.segreg[R_CS].val, R_SS);  //push CS
-    
-    cpu.esp -= 4;
-    swaddr_write(cpu.esp, 4, cpu.eip + 2, R_SS);  //push eip
 
     raise_intr(no);
 
