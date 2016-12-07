8a5e3d194c44c4f89fa37b980ae08a518c73b57b
tracer-ics2016 <tracer@njuics.org>
2016-12-07 13:08:09 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  13:08:09 up 20:05,  3 users,  load average: 0.00, 0.08, 0.08 406243ffed75dd7a18b7e99da51bb144d044f0c

--- testcase/src/test.c
+++ testcase/src/test.c
@@ -8,11 +8,8 @@ char buf[128];
 
 int main()
 {
-        buf[0] = '1';
-        buf[1] = '2';
-        buf[3] = 0;
-        printf("%s",buf);
-        //nemu_assert(strcmp(buf,"-1")==0);
+        sprintf(buf, "%d", -1);
+        nemu_assert(strcmp(buf,"-1")==0);
         return 0;
 }
 
