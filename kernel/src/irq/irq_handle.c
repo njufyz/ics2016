3daa1b02a72d444cb0ea6344860346b926dced5c
tracer-ics2016 <tracer@njuics.org>
2016-12-10 14:52:21 +0800

    >  compile kernel 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  14:52:21 up 1 day, 18:46,  3 users,  load average: 0.00, 0.00, 0.02 f8084de1ccc90951a8e02536a60d5af96fbe15fb

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -37,7 +37,6 @@ void irq_handle(TrapFrame *tf) {
     int irq = tf->irq;
 
 	if (irq < 0) {
-        Log("irq%d",irq);
         panic("Unhandled exception!");
 	} else if (irq == 0x80) {
         do_syscall(tf);
