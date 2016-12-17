21d3c4488f036b9463953e30be4fc1e90c9b1105
tracer-ics2016 <tracer@njuics.org>
2016-12-18 01:14:23 +0800

    >  compile game 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  01:14:23 up 3 days,  6:37,  3 users,  load average: 0.05, 0.05, 0.04 a0d908568ab86708a46e1348df49788b98269ae2

--- game/src/typing/draw.c
+++ game/src/typing/draw.c
@@ -38,11 +38,9 @@ redraw_screen() {
 	/* 绘制命中数、miss数、最后一次按键扫描码和fps */
 	
     draw_string(itoa(last_key_code()), SCR_HEIGHT - 8, 0, 48);
-//	hit = itoa(get_hit());
-	hit = "10";
+	hit = itoa(get_hit());
     draw_string(hit, 0, SCR_WIDTH - strlen(hit) * 8, 10);
-//	miss = itoa(get_miss());
-	miss = "10";
+	miss = itoa(get_miss());
     draw_string(miss, SCR_HEIGHT - 8, SCR_WIDTH - strlen(miss) * 8, 12);
 	draw_string(itoa(get_fps()), 0, 0, 14);
 	draw_string("FPS", 0, strlen(itoa(get_fps())) * 8, 14);
