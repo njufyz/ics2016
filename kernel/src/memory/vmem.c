ab96b495263f2fd156d26771d4ea0f284b219e25
tracer-ics2016 <tracer@njuics.org>
2016-12-18 00:20:44 +0800

    >  compile kernel 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  00:20:44 up 3 days,  5:43,  3 users,  load average: 0.00, 0.00, 0.00 e7d2d2179b6127fa81c8d72991b81ef882686333

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -16,9 +16,9 @@ void create_video_mapping() {
 	 * some page tables to create this mapping.
     */
     PDE * userdir = get_updir();
-    userdir->val = make_pde(va_to_pa((uptable)));
-    
     PTE*  usertable = uptable + (VMEM_ADDR >> 12);
+    userdir->val = make_pde(va_to_pa(usertable));
+    
     uint32_t pfram_addr;
     for(pfram_addr = VMEM_ADDR; pfram_addr < SCR_SIZE + VMEM_ADDR + PAGE_SIZE; pfram_addr += PAGE_SIZE, usertable ++){
         usertable -> val = make_pte(pfram_addr);
