657e9d3869fdbd9974e6a00996bfa44ea830440d
tracer-ics2016 <tracer@njuics.org>
2016-12-17 23:23:01 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  23:23:01 up 3 days,  4:45,  3 users,  load average: 0.01, 0.04, 0.00 833ade721abf3811182454c77b4ee39d1e6db825

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -89,6 +89,7 @@ void cpu_exec(volatile uint32_t n) {
             uint32_t intr_no = i8259_query_intr();
             i8259_ack_intr();
             raise_intr(intr_no);
+            Log("%x",intr_no);
         }
 #endif
 	}
