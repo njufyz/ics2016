989fb50e77378aff2d135902f9db4e6386b7a254
tracer-ics2016 <tracer@njuics.org>
2016-12-18 12:16:01 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:16:01 up 3 days,  8:32,  3 users,  load average: 0.47, 0.41, 0.29 619456ee1893589da0d56e759d9aeb2a81a88ac6

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -11,8 +11,8 @@ int main() {
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
 
-    set_bp();
-	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
+	HIT_GOOD_TRAP;
+    FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
 
 	FLOAT x1_ans = f2F(0.618);
