6be747ac52c3895d3c9a47216974b5bb8df5fcc5
tracer-ics2016 <tracer@njuics.org>
2016-12-18 12:22:14 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:22:14 up 3 days,  8:38,  3 users,  load average: 0.03, 0.15, 0.20 222402f752df600ed6bfd868d15d5c7f75b9007a

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -1,15 +1,17 @@
+#include <stdio.h>
 #include "trap.h"
 #include "FLOAT.h"
 
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
 
 int main() {
-	FLOAT a = f2F(0.5);
+    init_FLOAT_vfprintf();
+    FLOAT a = f2F(0.5);
 	FLOAT b = f2F(0.5);
 	FLOAT c = f2F(-0.5);
-
+    
+    printf("%f",FLOAT_ARG(F_mul_F(b, b)));
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
-	HIT_GOOD_TRAP;
 	FLOAT sqrt_dt = sqrt(dt);
 
     FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
