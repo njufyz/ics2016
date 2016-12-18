847a35f28538bafcb70f14aecb2877c87f5974dd
tracer-ics2016 <tracer@njuics.org>
2016-12-18 12:22:39 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:22:39 up 3 days,  8:38,  3 users,  load average: 0.21, 0.19, 0.21 f15d19309f4d0c6f176aecaf708b07284a57b82

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -10,7 +10,7 @@ int main() {
 	FLOAT b = f2F(0.5);
 	FLOAT c = f2F(-0.5);
     
-    printf("%f",FLOAT_ARG(F_mul_F(b, b)));
+    printf("%f\n",FLOAT_ARG(F_mul_F(b, b)));
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
 
