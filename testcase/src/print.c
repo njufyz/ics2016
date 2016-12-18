7c854b20fee076cd5503ae56cd6408becc906bb6
tracer-ics2016 <tracer@njuics.org>
2016-12-18 10:47:30 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:47:30 up 3 days,  7:58,  3 users,  load average: 0.06, 0.08, 0.09 5bbf132242ea3aec9c39c6f325a0a3d2e3e1f31e

--- testcase/src/print.c
+++ testcase/src/print.c
@@ -7,9 +7,8 @@
 int main() {
 	printf("%d + %d = %d\n", 1, 1, 2);
 
-	printf("%s", "Hello world!\n");
-    int x = f2F(7.9);
-    printf("%d",x);
+   // int x = f2F(7.9);
+   // printf("%d",x);
     printf("%d", 1);
     return 0;
 }
