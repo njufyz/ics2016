4c3c149d305faafe63650a6345fbe1c88bdb231f
tracer-ics2016 <tracer@njuics.org>
2016-12-18 10:43:26 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:43:26 up 3 days,  7:54,  3 users,  load average: 0.03, 0.08, 0.08 99b1f7dd6c3ff4eda5f365079fe0a7d640719eef

--- Makefile
+++ Makefile
@@ -53,7 +53,7 @@ clean: clean-cpp
 
 ##### some convinient rules #####
 
-USERPROG := obj/testcase/hello
+USERPROG := obj/testcase/print
 #USERPROG := $(game_BIN)
 #ENTRY := $(USERPROG)
 ENTRY := $(kernel_BIN)
--- testcase/src/print.c
+++ testcase/src/print.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 #include "FLOAT.h"
 
 #include "trap.h"
 
 
 int main() {
-    int x = f2F(7.9);
     printf("%d",1);
+    int x = f2F(7.9);
     printf("%d",x);
     return 0;
 }
