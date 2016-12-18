d72d66365098e38dc553e4818d26696c55eda98d
tracer-ics2016 <tracer@njuics.org>
2016-12-18 12:49:53 +0800

    >  run 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  12:49:53 up 3 days,  9:06,  3 users,  load average: 0.04, 0.03, 0.08 390e6d2ce28438d858bc7fbb47f1ec241761e42

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -10,7 +10,6 @@ int main() {
 	FLOAT b = f2F(0.5);
 	FLOAT c = f2F(-0.5);
     
-    nemu_assert(f2F(-0.5) == F_mul_F(int2F(1), c));
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
 
