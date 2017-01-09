36fc86252a7a5a80eca792e12f5574ce21dd0b2b
tracer-ics2016 <tracer@njuics.org>
2017-01-09 22:09:40 +0800

    >  compile NEMU 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  22:09:40 up 3 days, 12:13,  1 user,  load average: 0.17, 0.05, 0.01 fe95650e7b5e660d56c99df5e51df1cdb9fc416

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -6,59 +6,79 @@ enum {KEY_STATE_EMPTY, KEY_STATE_WAIT_RELEASE, KEY_STATE_RELEASE, KEY_STATE_PRES
 
 /* Only the following keys are used in NEMU-PAL. */
 static const int keycode_array[] = {
-	K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE,
-	K_RETURN, K_SPACE, K_PAGEUP, K_PAGEDOWN, K_r,
-	K_a, K_d, K_e, K_w, K_q,
-	K_s, K_f, K_p
+    K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE,
+    K_RETURN, K_SPACE, K_PAGEUP, K_PAGEDOWN, K_r,
+    K_a, K_d, K_e, K_w, K_q,
+    K_s, K_f, K_p
 };
 
 static int key_state[NR_KEYS];
 
 void
 keyboard_event(void) {
-	/* TODO: Fetch the scancode and update the key states. */
-   int key_code = in_byte(0x60);
-   int i = 0;
-   for(; i< NR_KEYS; i++)
-    assert(0);
+    /* TODO: Fetch the scancode and update the key states. */
+    int key_code = in_byte(0x60);
+    int i = 0;
+    for(; i< NR_KEYS; i++)
+    {
+        if(key_code & 0x7f == keycode_array[i])
+        {
+            if(key_code < 0x80) key_state[i] = KEY_STATE_PRESS;
+            else key_state[i] = KEY_STATE_RELEASE;
+        }
+    }
+
 }
 
 static inline int
 get_keycode(int index) {
-	assert(index >= 0 && index < NR_KEYS);
-	return keycode_array[index];
+    assert(index >= 0 && index < NR_KEYS);
+    return keycode_array[index];
 }
 
 static inline int
 query_key(int index) {
-	assert(index >= 0 && index < NR_KEYS);
-	return key_state[index];
+    assert(index >= 0 && index < NR_KEYS);
+    return key_state[index];
 }
 
 static inline void
 release_key(int index) {
-	assert(index >= 0 && index < NR_KEYS);
-	key_state[index] = KEY_STATE_WAIT_RELEASE;
+    assert(index >= 0 && index < NR_KEYS);
+    key_state[index] = KEY_STATE_WAIT_RELEASE;
 }
 
 static inline void
 clear_key(int index) {
-	assert(index >= 0 && index < NR_KEYS);
-	key_state[index] = KEY_STATE_EMPTY;
+    assert(index >= 0 && index < NR_KEYS);
+    key_state[index] = KEY_STATE_EMPTY;
 }
 
 bool 
 process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int)) {
-	cli();
-	/* TODO: Traverse the key states. Find a key just pressed or released.
-	 * If a pressed key is found, call `key_press_callback' with the keycode.
-	 * If a released key is found, call `key_release_callback' with the keycode.
-	 * If any such key is found, the function return true.
-	 * If no such key is found, the function return false.
-	 * Remember to enable interrupts before returning from the function.
-	 */
-
-	assert(0);
-	sti();
-	return false;
+    cli();
+    /* TODO: Traverse the key states. Find a key just pressed or released.
+     * If a pressed key is found, call `key_press_callback' with the keycode.
+     * If a released key is found, call `key_release_callback' with the keycode.
+     * If any such key is found, the function return true.
+     * If no such key is found, the function return false.
+     * Remember to enable interrupts before returning from the function.
+     */
+    int i = 0;
+    for(; i< NR_KEYS; i++)
+    {
+        if(key_state[i] == KEY_STATE_PRESS)
+        {
+            key_press_callback(get_keycode(i));
+            sti();
+            return true;
+        }
+        else if(key_state[i] == KEY_STATE_RELEASE)
+        {
+            key_release_callback(get_keycode(i));
+            sti();
+            return true;
+        }
+    sti();
+    return false;
 }
