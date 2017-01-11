26f8875bb2c12be7d43d984eb65b09a0a2a8514d
tracer-ics2016 <tracer@njuics.org>
2017-01-11 10:37:20 +0800

    >  compile game 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  10:37:20 up 4 days,  2:53,  2 users,  load average: 0.13, 0.07, 0.02 d0be592712a6ddff82a02914ef967ab858235d0a

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -44,7 +44,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect,
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 	assert(dst);
 	assert(color <= 0xff);
-
+    set_bp();
 	/* TODO: Fill the rectangle area described by `dstrect'
 	 * in surface `dst' with color `color'. If dstrect is
 	 * NULL, fill the whole surface.
