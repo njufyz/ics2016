a71d8ce768143ff29f51c76ebf3c588958515006
tracer-ics2016 <tracer@njuics.org>
2017-01-11 11:14:31 +0800

    >  compile game 151220026 njufyz Linux 2d8bfc8bc3a4 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  11:14:31 up 4 days,  3:30,  2 users,  load average: 0.13, 0.27, 0.20 ed378a0aaed96ab7d3d586fdd54980a490ecd6ff

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -87,7 +87,7 @@ void SDL_SetPalette(SDL_Surface *s, int flags, SDL_Color *colors,
 
 	if(s->flags & SDL_HWSURFACE) {
 		/* TODO: Set the VGA palette by calling write_palette(). */
-		write_palette(s->format->palette->colors, s->format->palette->ncolors);
+		write_palette(s->format->palette->colors, ncolors);
      //   assert(0);
 	}
 }
